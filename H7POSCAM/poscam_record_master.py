58dfcb190fde7717bf7f0748b762f393945da4d9
Phuong Phung <pkp013@gmail.com>
2023-04-06 11:52:18 +0700

    poscam rec

--- H7POSCAM/poscam_record_master.py
+++ H7POSCAM/poscam_record_master.py
@@ -1,35 +1,48 @@
-import sensor, image, time, pyb,mjpeg
+# MJPEG Video Recording Example
+#
+# Note: You will need an SD card to run this demo.
+#
+# You can use your OpenMV Cam to record mjpeg files. You can either feed the
+# recorder object JPEG frames or RGB565/Grayscale frames. Once you've finished
+# recording a Mjpeg file you can use VLC to play it. If you are on Ubuntu then
+# the built-in video player will work too.
+
+import sensor, image, time, mjpeg, pyb
+
 RED_LED_PIN = 1
 BLUE_LED_PIN = 3
 pyb.LED(RED_LED_PIN).on()
-sensor.reset()                      # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
-sensor.set_framesize(sensor.HD)   # Set frame size to QVGA (320x240)
-#sensor.set_windowing((0, 0, 160, 120))
-sensor.skip_frames(time = 2000)     # Wait for settings take effect.
-clock = time.clock()                # Create a clock object to track the FPS.
+pyb.LED(BLUE_LED_PIN).off()
+sensor.reset() # Initialize the camera sensor.
+sensor.set_pixformat(sensor.GRAYSCALE) # or sensor.GRAYSCALE
+sensor.set_framesize(sensor.HD) # or sensor.QQVGA (or others)
+sensor.skip_frames(time = 2000) # Let new settings take affect.
+clock = time.clock() # Tracks FPS.
 uart = pyb.UART(3, 500000)
+pyb.LED(RED_LED_PIN).on()
+sensor.skip_frames(time = 2000) # Give the user time to get ready.
+
 pyb.LED(RED_LED_PIN).off()
-#img_writer = image.ImageIO(str(pyb.rng())+'.img','w')
-m = mjpeg.Mjpeg(str(pyb.rng())+".mjpeg")
-i = 0
-count = 0
-fps = 14
-mspf = round(1000/fps)
 pyb.LED(BLUE_LED_PIN).on()
-while (count<100):
+filename = str(pyb.rng())
+m = mjpeg.Mjpeg(filename+"ms.mjpeg")
+uart.write('$'+filename+'@')
+count = 0
+fps = 20
+mspf = round(1000/fps)
+sensor.skip_frames(time = 100)
+for i in range(4200):
     frame_start = pyb.millis()
+    clock.tick()
     uart.write('$'+str(count)+'#')
-    #clock.tick()
-    img = sensor.snapshot()
-    m.add_frame(sensor.snapshot(),quality = 50)
+    m.add_frame(sensor.snapshot())
     frame_end  = pyb.millis()
     delta_time = frame_end - frame_start
-
     count=count+1
     if (delta_time < mspf):
         pyb.delay(mspf - delta_time-1)
-    #print(clock.fps())
-m.close(14)
+    print(clock.fps())
+uart.write('!')
+m.close(fps)
 pyb.LED(BLUE_LED_PIN).off()
-#img_writer.close()
+print("Done! Reset the camera to see the saved recording.")
--- H7POSCAM/poscam_record_slave.py
+++ H7POSCAM/poscam_record_slave.py
@@ -4,7 +4,7 @@
 #
 # You can use your OpenMV Cam to save image files.
 
-import sensor, image, pyb,time
+import sensor, image, pyb,time,mjpeg
 from pyb import Pin, Timer
 
 RED_LED_PIN = 1
@@ -14,26 +14,33 @@ sensor.reset()                      # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.HD) # or sensor.QQVGA (or others)
 sensor.skip_frames(time = 1000) # Let new settings take affect.
-m = mjpeg.Mjpeg(str(pyb.rng())+".mjpeg")
+m = mjpeg.Mjpeg("abs.mjpeg")
 pyb.LED(RED_LED_PIN).on()
 uart = pyb.UART(3, 500000)
-
+clock = time.clock() # Tracks FPS.
 pyb.LED(RED_LED_PIN).off()
-pyb.LED(BLUE_LED_PIN).on()
-
+framid = ""
+count=0
 
 while (True):
     inputint = uart.readchar()
     if(inputint>=0):
-        bytein = chr()
+        bytein = chr(inputint)
+
         if(bytein=="$"):
             framid = ""
         elif (bytein=="#"):
-            m.add_frame(sensor.snapshot(),quality = 50)
+            #clock.tick()
+            #print(framid)
+            m.add_frame(sensor.snapshot())
+            #count=count+1
+            #print(count)
         elif (bytein=="!"):
             break
+        elif (bytein=="@"):
+            m = mjpeg.Mjpeg(framid+"sl.mjpeg")
+            pyb.LED(BLUE_LED_PIN).on()
         else:
             framid = framid+bytein
-        #pyb.LED(BLUE_LED_PIN).off()
-m.close(14)
+m.close(20)
 pyb.LED(BLUE_LED_PIN).off()
