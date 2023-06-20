1fdd680ce012cc0567ec7c97505e3e54a71545ca
Phuong Phung <pkp013@gmail.com>
2023-06-20 23:41:45 +0700

    kk

--- AGV_MAGNETO/agv_magnet_rp2040/agv_magnet_rp2040.ino
+++ AGV_MAGNETO/agv_magnet_rp2040/agv_magnet_rp2040.ino
@@ -1,15 +1,42 @@
 #define RXPIN 5
 #define TXPIN 4
-UART rfidReader( TXPIN, RXPIN,0, 0);
-byte msg_setup[] = {0x01, 0x06, 0x00, 0x2F, 0x00, 0x05, 0x78, 0x00};
+UART magneto( TXPIN, RXPIN, 0, 0);
+byte msg_setup[] = {0x01, 0x03, 0x00, 0x20, 0x00, 0x08, 0x45, 0xC6};
 void setup() {
   // put your setup code here, to run once:
-  rfidReader.begin(9600);
-  
-}
+  magneto.begin(115200);
+  Serial.begin(115200);
 
+}
+byte inputBuff[20];
 void loop() {
   // put your main code here, to run repeatedly:
-  rfidReader.write(msg_setup,8);
-  delay(500);
+  delay(5);
+  magneto.write(msg_setup,8);delay(5);
+  int i=0;
+  while (magneto.available())
+  {
+    inputBuff[i] = magneto.read();
+    i++;
+  }
+  if((inputBuff[0]==0x01)&&(inputBuff[1]==0x03)&&(inputBuff[2]==0x10))
+  {
+    float sum=0;
+    int max_index=0;
+    int max_value=0;
+    for(i=3;i<19;i++)
+    {
+      sum+= (inputBuff[i]);
+      if(max_value<inputBuff[i])
+      {
+        max_value = inputBuff[i];
+        max_index=i-3;
+      }
+      }
+      float avrg = sum/16.0;
+      if(max_value>(3+avrg*1.5))
+      Serial.println(max_index);
+      else Serial.println(8);
+  }
+  
 }
--- hoptiepdan/hoptiepdan.ino
+++ hoptiepdan/hoptiepdan.ino
@@ -15,7 +15,8 @@ int led2 = 25;
 int led3 = 21;
 int led4 = 16;
 int laser =27;
-int dcmotor=14;
+int trig=33;
+int dcmotor=14;int high_volt=0;
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize the digital pin as an output.
@@ -24,25 +25,42 @@ void setup() {
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
   pinMode(laser, OUTPUT);
+  pinMode(trig, INPUT);
   pinMode(dcmotor, OUTPUT);
   digitalWrite(dcmotor, LOW); 
   
 }
-
-// the loop routine runs over and over again forever:
-void loop() {
-  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(led2, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(led3, LOW);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(led4, LOW);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(laser, LOW); 
-  digitalWrite(dcmotor, LOW); 
-  delay(1000);               // wait for a second
+void onTrig()
+{  
   digitalWrite(led1, LOW);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(led2, LOW);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(led3, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(led4, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(laser, HIGH); 
+  digitalWrite(high_volt, HIGH); 
   digitalWrite(dcmotor, HIGH); 
-  delay(100);               // wait for a second
+  delay(50);
+  offTrig();
+   
+  }
+  void offTrig()
+{
+    digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(led2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(led3, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(led4, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(laser, LOW); 
+  digitalWrite(high_volt, LOW); 
+  digitalWrite(dcmotor, LOW); 
+  delay(100);
+  }
+// the loop routine runs over and over again forever:
+void loop() {
+  while (digitalRead(trig)!=0){};
+  onTrig();
+  while (digitalRead(trig)!=0){};
+  onTrig();
+  while (digitalRead(trig)!=0){};
+  onTrig();
+  while (digitalRead(trig)==0){};
 }
