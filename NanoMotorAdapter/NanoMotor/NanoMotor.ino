a2003b42908518623fd43844c286675f899d2434
Phuong Phung <pkp013@gmail.com>
2023-12-14 14:16:18 +0700

    Update NanoMotor.ino

--- NanoMotorAdapter/NanoMotor/NanoMotor.ino
+++ NanoMotorAdapter/NanoMotor/NanoMotor.ino
@@ -1,19 +1,77 @@
-#define REW     2
+#define MEN     2
 #define FWD     3
 #define ALM_RES 4
 #define SPEED   5
 #define MB_FREE 6
-#define SPD_IN  7
-#define WNG_IN 8
+#define MIN_LIM 7
+#define WNG_IN  8
 #define ALARM_IN 9
-#define VH_OK 10
+#define MAX_LIM 10
 #define LED_1 11
+#define DEBUG_TELEMETRY Serial
+#define COMMAND_LEN_MAX 100
+int output_speed;
+int stackLevel=0;
+String commandString;
+bool isMin,isMax;
+void setSpeed(int speed);
+void updateCommandBus()
+{
+  while(DEBUG_TELEMETRY.available())
+  {
+    uint8_t bytein = DEBUG_TELEMETRY.read();
+    
+    if(commandString.length()<COMMAND_LEN_MAX)commandString+=(char)bytein;
+    if(bytein=='\n')//end of command
+    {
+      int dataLen = commandString.length();
+      if(commandString.startsWith("TRS="))//angle set command
+      {
+        float speed = commandString.substring(4,dataLen-1).toFloat();
+        setSpeed(speed);
+        DEBUG_TELEMETRY.print("Speed set ok:");
+        DEBUG_TELEMETRY.println(output_speed);
+      }
+      
+      commandString = "";
+      
+    }
+      
+  }
+}
+
+void setSpeed(int speed)
+{
+  output_speed=speed;
+  if(speed>0){
+    digitalWrite(FWD,HIGH);
+    analogWrite(SPEED, 255-speed);
+  }
+  else
+  {
+    digitalWrite(FWD,LOW);
+    analogWrite(SPEED, 255+speed);
+  }
+}
 void setup() {
-  // put your setup code here, to run once:
-  
+  pinMode(MIN_LIM,INPUT);     
+  pinMode(WNG_IN,INPUT);      
+  pinMode(ALARM_IN,INPUT);    
+  pinMode(MAX_LIM,INPUT);       
+  pinMode(LED_1,OUTPUT);      digitalWrite(LED_1,HIGH);
+  pinMode(LED_BUILTIN,OUTPUT);digitalWrite(LED_BUILTIN,LOW);
+  pinMode(MEN,OUTPUT);        digitalWrite(MEN,LOW);
+  pinMode(FWD,OUTPUT);        digitalWrite(FWD,LOW);
+  pinMode(ALM_RES,OUTPUT);    digitalWrite(ALM_RES,LOW);
+  pinMode(SPEED,OUTPUT);      digitalWrite(SPEED,HIGH);
+  pinMode(MB_FREE,OUTPUT);    digitalWrite(MB_FREE,HIGH);
+  setSpeed(0);
+  Serial.begin(500000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  updateCommandBus();
+  isMin = digitalRead(MIN_LIM);
+  isMax = digitalRead(MAX_LIM);
+DEBUG_TELEMETRY.println("Motor Ready");
 }
