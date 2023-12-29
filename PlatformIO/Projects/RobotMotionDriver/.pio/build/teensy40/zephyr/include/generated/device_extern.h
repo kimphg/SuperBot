35157a7fdb23843ba06ffca8f7aaf4480e1b3876
Phuong Phung <pkp013@gmail.com>
2023-12-29 07:52:28 +0700

    nano motor added and teensy move to platform IO

---
+++ PlatformIO/Projects/RobotMotionDriver/.pio/build/teensy40/zephyr/include/generated/device_extern.h
@@ -0,0 +1,95 @@
+#ifndef DEVICE_EXTERN_GEN_H
+#define DEVICE_EXTERN_GEN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const struct device DEVICE_DT_NAME_GET(DT_N); /* dts_ord_0 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_aliases); /* dts_ord_1 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_chosen); /* dts_ord_2 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus); /* dts_ord_3 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_0); /* dts_ord_4 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_0_S_mpu_e000ed90); /* dts_ord_5 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_leds); /* dts_ord_6 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc); /* dts_ord_7 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_interrupt_controller_e000e100); /* dts_ord_8 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_gpio_401bc000); /* dts_ord_9 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_leds_S_led_1); /* dts_ord_10 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_ccm_400fc000); /* dts_ord_11 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_can_401d0000); /* dts_ord_12 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_can_401d4000); /* dts_ord_13 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_can_401d8000); /* dts_ord_14 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_csi_402bc000); /* dts_ord_15 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_display_controller_402b8000); /* dts_ord_16 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_dma_controller_400e8000); /* dts_ord_17 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_gpio_400c0000); /* dts_ord_18 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_gpio_401b8000); /* dts_ord_19 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_gpio_401c0000); /* dts_ord_20 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_gpio_401c4000); /* dts_ord_21 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_gpt_401ec000); /* dts_ord_22 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_gpt_401f0000); /* dts_ord_23 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_i2c_403f0000); /* dts_ord_24 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_i2c_403f4000); /* dts_ord_25 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_i2c_403f8000); /* dts_ord_26 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_i2c_403fc000); /* dts_ord_27 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_iomuxc_401f8000); /* dts_ord_28 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_random_400cc000); /* dts_ord_29 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_semc0_402f0000); /* dts_ord_30 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_spi_402a4000); /* dts_ord_31 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_spi_40394000); /* dts_ord_32 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_spi_40398000); /* dts_ord_33 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_spi_4039c000); /* dts_ord_34 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_spi_403a0000); /* dts_ord_35 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_timer_e000e010); /* dts_ord_36 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_uart_40184000); /* dts_ord_37 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_uart_40188000); /* dts_ord_38 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_uart_4018c000); /* dts_ord_39 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_uart_40190000); /* dts_ord_40 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_uart_40194000); /* dts_ord_41 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_uart_40198000); /* dts_ord_42 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_uart_4019c000); /* dts_ord_43 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_uart_401a0000); /* dts_ord_44 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_system_clock); /* dts_ord_45 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_usbd_402e0000); /* dts_ord_46 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_usbd_402e0200); /* dts_ord_47 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_usdhc_402c0000); /* dts_ord_48 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_usdhc_402c4000); /* dts_ord_49 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_wdog_400b8000); /* dts_ord_50 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_wdog_400d0000); /* dts_ord_51 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_ethernet_402d4000); /* dts_ord_52 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_ethernet_402d4000_S_ptp); /* dts_ord_53 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_ethernet_402d8000); /* dts_ord_54 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_ethernet_402d8000_S_ptp); /* dts_ord_55 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403dc000); /* dts_ord_56 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403dc000_S_pwm0); /* dts_ord_57 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403dc000_S_pwm1); /* dts_ord_58 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403dc000_S_pwm2); /* dts_ord_59 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403dc000_S_pwm3); /* dts_ord_60 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e0000); /* dts_ord_61 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e0000_S_pwm0); /* dts_ord_62 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e0000_S_pwm1); /* dts_ord_63 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e0000_S_pwm2); /* dts_ord_64 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e0000_S_pwm3); /* dts_ord_65 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e4000); /* dts_ord_66 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e4000_S_pwm0); /* dts_ord_67 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e4000_S_pwm1); /* dts_ord_68 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e4000_S_pwm2); /* dts_ord_69 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e4000_S_pwm3); /* dts_ord_70 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e8000); /* dts_ord_71 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e8000_S_pwm0); /* dts_ord_72 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e8000_S_pwm1); /* dts_ord_73 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e8000_S_pwm2); /* dts_ord_74 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexpwm_403e8000_S_pwm3); /* dts_ord_75 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexram_400b0000); /* dts_ord_76 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexram_400b0000_S_dtcm_20000000); /* dts_ord_77 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexram_400b0000_S_itcm_0); /* dts_ord_78 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_flexram_400b0000_S_ocram_20200000); /* dts_ord_79 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_spi_402a8000); /* dts_ord_80 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_spi_402a8000_S_w25q16jvuxim_0); /* dts_ord_81 */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DEVICE_EXTERN_GEN_H */
