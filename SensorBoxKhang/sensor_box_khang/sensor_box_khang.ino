c18752ecc4c44efc6729ccbe9061a47663e4e60b
Phuong Phung <pkp013@gmail.com>
2026-02-02 22:11:48 +0700

    Update sensor_box_khang.ino

--- SensorBoxKhang/sensor_box_khang/sensor_box_khang.ino
+++ SensorBoxKhang/sensor_box_khang/sensor_box_khang.ino
@@ -2,25 +2,45 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
-
+#include <DHT.h>
+#define PIN_DHT       2          // PD2 (INT0)
+#define PIN_PIR       4          // PD4
+#define PIN_LED_CTRL  A2         // PC2 / ADC2 -> LEDControl (GP2Y10)
+#define PIN_DUST_AO   A3         // PC3 / ADC3 -> ANALOG_OUTPUT (GP2Y10)
+#define PIN_MQ135     A0         // PC0 / ADC0
+#define PIN_NOISE     A1         // PC1 / ADC1
+#define DHTTYPE DHT22
+#define ETH_RST 4
+DHT dht(PIN_DHT, DHTTYPE);
 // the media access control (ethernet hardware) address for the shield:
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };  
 //the IP address for the shield:
 byte ip[] = { 192, 168, 100, 10 };  
-char ipRemote []      = "192.168.100.15"; 
+char ipRemote []      = "192.168.100.221"; 
 int portRemote        = 2021;
 IPAddress myDns(192, 168, 100, 1);
 IPAddress gateway(192, 168, 100, 1);
 IPAddress subnet(255, 255, 255, 0);
 unsigned int localPort = 8888;
+static uint16_t sampleDustRaw() {
+  analogWrite(PIN_LED_CTRL, 255);   // bật LED
+  delayMicroseconds(280);
+  uint16_t val = analogRead(PIN_DUST_AO);
+  delayMicroseconds(40);
+  analogWrite(PIN_LED_CTRL, 0);     // tắt LED
+  delayMicroseconds(9680);          //  10ms chu kỳ
+  return val;
+}
 // buffers for receiving and sending data
 #define UDP_TX_PACKET_MAX_SIZE 500
 EthernetUDP Udp;
 char inBuffer[UDP_TX_PACKET_MAX_SIZE];  // buffer to hold incoming packet,
-char outBuffer[50];
+char outBuffer[200];
 void setup() {
   // put your setup code here, to run once:
   Ethernet.init(10);
+  pinMode(ETH_RST,OUTPUT);
+  digitalWrite(ETH_RST,HIGH);
   Ethernet.begin(mac, ip, myDns, gateway, subnet);
   // Check for Ethernet hardware present
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
@@ -40,9 +60,22 @@ long randNumber;
 void loop() {
   // put your main code here, to run repeatedly:
  delay(200);
+
+
+  // Nếu đọc lỗi DHT, trả NaN – vẫn gửi để phía server biết
+  uint16_t dust = sampleDustRaw();        // 0..1023
+  uint16_t mq135 = analogRead(PIN_MQ135);
+  uint16_t noise = analogRead(PIN_NOISE);
+  int pir = digitalRead(PIN_PIR);
+  const float Vref = 5.0;
+  float dustV  = dust  * Vref / 1023.0;
+  float mqV    = mq135 * Vref / 1023.0;
+  float noiseV = noise * Vref / 1023.0;
+
  randNumber = random(300);
- int temp = randNumber/30.0+15.0;
- int hum = randNumber/20.0+50.0;
+ uint16_t temp = dht.readTemperature();
+ uint16_t hum = dht.readHumidity();
+
  int move = randNumber>250;
  
  Serial.print("$SENS,ID,");
@@ -78,11 +111,12 @@ void loop() {
     // send a reply to the IP address and port that sent us the packet we received
     
   }
-  outBuffer[0]=0xf1;
-  memcpy(&outBuffer[1],(char*)&temp,4);
-  memcpy(&outBuffer[5],(char*)&hum,4);
-  memcpy(&outBuffer[9],(char*)&move,4);
+  sprintf(outBuffer, "{\"id\": %d, \"temperature\": %d, \"humidity\": %d, \"pm25\": %d, \"noise\": %d, \"mq135\": %d }", 1, int(temp),int(hum),int(dust),int(noise),int(mq135));
+  // outBuffer[0]=0xf1;
+  // memcpy(&outBuffer[1],(char*)&temp,4);
+  // memcpy(&outBuffer[5],(char*)&hum,4);
+  // memcpy(&outBuffer[9],(char*)&move,4);
   Udp.beginPacket(ipRemote,portRemote);
-  Udp.write(outBuffer,13);
+  Udp.write(outBuffer,strlen(outBuffer));
   Udp.endPacket();
 }
