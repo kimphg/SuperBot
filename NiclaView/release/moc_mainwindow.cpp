17a08b72d56ae391a8afce49f3746dbe2b391647
Phuong Phung <pkp013@gmail.com>
2026-06-03 07:37:49 +0700

    Update moc_mainwindow.cpp

--- NiclaView/release/moc_mainwindow.cpp
+++ NiclaView/release/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[13];
-    char stringdata0[202];
+    QByteArrayData data[14];
+    char stringdata0[219];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -41,17 +41,19 @@ QT_MOC_LITERAL(5, 42, 12), // "videoReceive"
 QT_MOC_LITERAL(6, 55, 21), // "on_pushButton_clicked"
 QT_MOC_LITERAL(7, 77, 14), // "udpDataReceive"
 QT_MOC_LITERAL(8, 92, 13), // "sendHeartbeat"
-QT_MOC_LITERAL(9, 106, 23), // "on_pushButton_2_clicked"
-QT_MOC_LITERAL(10, 130, 23), // "on_pushButton_3_clicked"
-QT_MOC_LITERAL(11, 154, 23), // "on_pushButton_4_clicked"
-QT_MOC_LITERAL(12, 178, 23) // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(9, 106, 16), // "discoveryReceive"
+QT_MOC_LITERAL(10, 123, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(11, 147, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(12, 171, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(13, 195, 23) // "on_pushButton_5_clicked"
 
     },
     "MainWindow\0timerEvent\0\0QTimerEvent*\0"
     "event\0videoReceive\0on_pushButton_clicked\0"
     "udpDataReceive\0sendHeartbeat\0"
-    "on_pushButton_2_clicked\0on_pushButton_3_clicked\0"
-    "on_pushButton_4_clicked\0on_pushButton_5_clicked"
+    "discoveryReceive\0on_pushButton_2_clicked\0"
+    "on_pushButton_3_clicked\0on_pushButton_4_clicked\0"
+    "on_pushButton_5_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -61,7 +63,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       9,   14, // methods
+      10,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -69,18 +71,20 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   59,    2, 0x09 /* Protected */,
-       5,    0,   62,    2, 0x08 /* Private */,
-       6,    0,   63,    2, 0x08 /* Private */,
-       7,    0,   64,    2, 0x08 /* Private */,
-       8,    0,   65,    2, 0x08 /* Private */,
-       9,    0,   66,    2, 0x08 /* Private */,
-      10,    0,   67,    2, 0x08 /* Private */,
-      11,    0,   68,    2, 0x08 /* Private */,
-      12,    0,   69,    2, 0x08 /* Private */,
+       1,    1,   64,    2, 0x09 /* Protected */,
+       5,    0,   67,    2, 0x08 /* Private */,
+       6,    0,   68,    2, 0x08 /* Private */,
+       7,    0,   69,    2, 0x08 /* Private */,
+       8,    0,   70,    2, 0x08 /* Private */,
+       9,    0,   71,    2, 0x08 /* Private */,
+      10,    0,   72,    2, 0x08 /* Private */,
+      11,    0,   73,    2, 0x08 /* Private */,
+      12,    0,   74,    2, 0x08 /* Private */,
+      13,    0,   75,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -104,10 +108,11 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 2: _t->on_pushButton_clicked(); break;
         case 3: _t->udpDataReceive(); break;
         case 4: _t->sendHeartbeat(); break;
-        case 5: _t->on_pushButton_2_clicked(); break;
-        case 6: _t->on_pushButton_3_clicked(); break;
-        case 7: _t->on_pushButton_4_clicked(); break;
-        case 8: _t->on_pushButton_5_clicked(); break;
+        case 5: _t->discoveryReceive(); break;
+        case 6: _t->on_pushButton_2_clicked(); break;
+        case 7: _t->on_pushButton_3_clicked(); break;
+        case 8: _t->on_pushButton_4_clicked(); break;
+        case 9: _t->on_pushButton_5_clicked(); break;
         default: ;
         }
     }
@@ -142,13 +147,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 9)
+        if (_id < 10)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 9;
+        _id -= 10;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 9)
+        if (_id < 10)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 9;
+        _id -= 10;
     }
     return _id;
 }
