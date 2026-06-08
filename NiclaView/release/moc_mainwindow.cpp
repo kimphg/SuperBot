27c21058638a062d433887a22e4265e7e9f887d7
Phuong Phung <pkp013@gmail.com>
2026-06-08 11:04:06 +0700

    s

--- NiclaView/release/moc_mainwindow.cpp
+++ NiclaView/release/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[14];
-    char stringdata0[219];
+    QByteArrayData data[12];
+    char stringdata0[190];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,21 +37,18 @@ QT_MOC_LITERAL(1, 11, 10), // "timerEvent"
 QT_MOC_LITERAL(2, 22, 0), // ""
 QT_MOC_LITERAL(3, 23, 12), // "QTimerEvent*"
 QT_MOC_LITERAL(4, 36, 5), // "event"
-QT_MOC_LITERAL(5, 42, 12), // "videoReceive"
-QT_MOC_LITERAL(6, 55, 21), // "on_pushButton_clicked"
-QT_MOC_LITERAL(7, 77, 14), // "udpDataReceive"
-QT_MOC_LITERAL(8, 92, 13), // "sendHeartbeat"
-QT_MOC_LITERAL(9, 106, 16), // "discoveryReceive"
-QT_MOC_LITERAL(10, 123, 23), // "on_pushButton_2_clicked"
-QT_MOC_LITERAL(11, 147, 23), // "on_pushButton_3_clicked"
-QT_MOC_LITERAL(12, 171, 23), // "on_pushButton_4_clicked"
-QT_MOC_LITERAL(13, 195, 23) // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(5, 42, 13), // "serialReceive"
+QT_MOC_LITERAL(6, 56, 15), // "retryConnection"
+QT_MOC_LITERAL(7, 72, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(8, 94, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(9, 118, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(10, 142, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(11, 166, 23) // "on_pushButton_5_clicked"
 
     },
     "MainWindow\0timerEvent\0\0QTimerEvent*\0"
-    "event\0videoReceive\0on_pushButton_clicked\0"
-    "udpDataReceive\0sendHeartbeat\0"
-    "discoveryReceive\0on_pushButton_2_clicked\0"
+    "event\0serialReceive\0retryConnection\0"
+    "on_pushButton_clicked\0on_pushButton_2_clicked\0"
     "on_pushButton_3_clicked\0on_pushButton_4_clicked\0"
     "on_pushButton_5_clicked"
 };
@@ -63,7 +60,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      10,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -71,21 +68,17 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   64,    2, 0x09 /* Protected */,
-       5,    0,   67,    2, 0x08 /* Private */,
-       6,    0,   68,    2, 0x08 /* Private */,
-       7,    0,   69,    2, 0x08 /* Private */,
-       8,    0,   70,    2, 0x08 /* Private */,
-       9,    0,   71,    2, 0x08 /* Private */,
-      10,    0,   72,    2, 0x08 /* Private */,
-      11,    0,   73,    2, 0x08 /* Private */,
-      12,    0,   74,    2, 0x08 /* Private */,
-      13,    0,   75,    2, 0x08 /* Private */,
+       1,    1,   54,    2, 0x09 /* Protected */,
+       5,    0,   57,    2, 0x08 /* Private */,
+       6,    0,   58,    2, 0x08 /* Private */,
+       7,    0,   59,    2, 0x08 /* Private */,
+       8,    0,   60,    2, 0x08 /* Private */,
+       9,    0,   61,    2, 0x08 /* Private */,
+      10,    0,   62,    2, 0x08 /* Private */,
+      11,    0,   63,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, 0x80000000 | 3,    4,
-    QMetaType::Void,
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -104,15 +97,13 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->timerEvent((*reinterpret_cast< QTimerEvent*(*)>(_a[1]))); break;
-        case 1: _t->videoReceive(); break;
-        case 2: _t->on_pushButton_clicked(); break;
-        case 3: _t->udpDataReceive(); break;
-        case 4: _t->sendHeartbeat(); break;
-        case 5: _t->discoveryReceive(); break;
-        case 6: _t->on_pushButton_2_clicked(); break;
-        case 7: _t->on_pushButton_3_clicked(); break;
-        case 8: _t->on_pushButton_4_clicked(); break;
-        case 9: _t->on_pushButton_5_clicked(); break;
+        case 1: _t->serialReceive(); break;
+        case 2: _t->retryConnection(); break;
+        case 3: _t->on_pushButton_clicked(); break;
+        case 4: _t->on_pushButton_2_clicked(); break;
+        case 5: _t->on_pushButton_3_clicked(); break;
+        case 6: _t->on_pushButton_4_clicked(); break;
+        case 7: _t->on_pushButton_5_clicked(); break;
         default: ;
         }
     }
@@ -147,13 +138,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 10)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 10;
+        _id -= 8;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 10)
+        if (_id < 8)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 10;
+        _id -= 8;
     }
     return _id;
 }
